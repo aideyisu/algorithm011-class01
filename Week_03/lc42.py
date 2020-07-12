81c460d03123145ba7fd59864dde13143ac5f494
aideyisu <740531307@qq.com>
2020-07-12 23:37:28 +0800

    Week03

---
+++ Week_03/lc42.py
@@ -0,0 +1,19 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        length = len(height)
+        if length < 3:
+            return 0
+        
+        res, idx = 0, 0
+        stack = []
+        while idx < length:
+            while len(stack) > 0 and height[idx] > height[stack[-1]]:
+                top = stack.pop()
+                if len(stack) == 0:
+                    break
+                h = min(height[stack[-1]], height[idx]) - height[top]
+                dist = idx - stack[-1] -1
+                res += (dist * h)
+            stack.append(idx)
+            idx += 1
+        return res
